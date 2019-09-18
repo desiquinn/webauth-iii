38ac6e35a350ea2696d019c12e4120c17f483bbf
Desiree Morris <desiquinncompany@gmail.com>
2019-09-18 19:40:29 -0400

    hit mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const AuthRouter = require('../auth/auth_router.js');
+const UsersRouter = require('../users/users_router');
 
 server = express();
 
 server.use(express.json());
+server.use('/api/auth', AuthRouter);
+server.use('/api/users', UsersRouter);
 
 module.exports = server;
